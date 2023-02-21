e4c844b786ea91697014eff173ca85b87edb39a8
Frank Assafuah <frankassafuah@gmail.com>
2023-02-21 23:46:55 +0000

    components and layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    
+    <incAppHeader />
+    <Nuxt />
+    <incAppFooter />
   </div>
 </template>
 
 <script>
 export default {
-  name: "IndexPage",
+  name: "default",
 };
 </script>
