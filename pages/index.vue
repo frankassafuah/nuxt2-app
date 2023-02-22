4e751ea9a3352036f23825e45631b82d343e3a1c
Frank Assafuah <frankassafuah@gmail.com>
2023-02-22 22:08:14 +0000

    Creeated pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    
+  <div class="container">
+    <h1>Home page</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "IndexPage",
+  name: "home",
 };
 </script>
