9bf8bad6f63f1dbdf42ab73f5502b8f8a73af44d
Frank Assafuah <frankassafuah@gmail.com>
2023-02-24 22:41:44 +0000

    Design home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,81 @@
 <template>
   <div>
     <carousel />
-    <div class="container">
-      <h1>Hoempage</h1>
+    <div class="py-5 container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h1 class="main-heading">About us</h1>
+          <div class="underline mx-auto"></div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit,
+            quisquam. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Officia, excepturi voluptates! Eveniet dolore laborum id! Lorem
+            ipsum, dolor sit amet consectetur adipisicing elit. Officia,
+            excepturi voluptates! Eveniet dolore laborum id!
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="py-5 bg-gray">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1 class="main-heading">Our Services</h1>
+            <div class="underline mx-auto"></div>
+          </div>
+          <div class="col-md-4">
+            <div class="box-card">
+              <img
+              style="width: 350px; height: 300px;"
+                src="/images/image-from-rawpixel-id-381821-jpeg.jpg"
+                alt=""
+              />
+              <div class="box-card-body">
+                <h3 class="box-card-heading">Web Designing</h3>
+                <div class="underline"></div>
+                <p>
+                  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                  consectetur adipisicing.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="box-card">
+              <img
+              style="width: 350px; height: 300px;"
+                src="/images/image-from-rawpixel-id-381821-jpeg.jpg"
+                alt=""
+              />
+              <div class="box-card-body">
+                <h3 class="box-card-heading">Web Designing</h3>
+                <div class="underline"></div>
+                <p>
+                  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                  consectetur adipisicing.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="box-card">
+              <img
+              style="width: 350px; height: 300px;"
+                src="/images/image-from-rawpixel-id-381821-jpeg.jpg"
+                alt=""
+              />
+              <div class="box-card-body">
+                <h3 class="box-card-heading">Web Designing</h3>
+                <div class="underline"></div>
+                <p>
+                  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                  consectetur adipisicing.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
